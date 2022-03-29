db082c027ffbf2ca838b9ecd918f5a60e070a0aa
Seul An Kim <sakim5027@gmail.com>
2022-03-29 16:25:40 -0400

    files cleared

--- client/src/_actions/types.js
+++
@@ -1,5 +0,0 @@
-export const LOGIN_USER = 'login_user';
-export const GOOGLE_LOGIN = 'google_login';
-export const REGISTER_USER = 'register_user';
-export const LOGOUT_USER = 'logout_user';
-export const AUTH_USER = 'auth_user';
